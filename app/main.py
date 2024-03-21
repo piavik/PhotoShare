1ca8a6d26b5582a574df34590c0289e8dadeedb1
RomanSiu <siusiailoroman@gmail.com>
2024-03-21 12:50:18 +0200

    add models (without roles)

--- app/main.py
+++ app/main.py
@@ -5,9 +5,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"PhotoShare": "FastAPI group project"}
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
---
+++ app/src/database/models.py
@@ -0,0 +1,54 @@
+from sqlalchemy import String, DateTime, ForeignKey, Table, Column, Boolean
+from sqlalchemy.orm import declarative_base, mapped_column, Mapped, relationship
+from datetime import datetime
+
+
+Base = declarative_base()
+
+association_table = Table(
+    "association_table",
+    Base.metadata,
+    Column("photos", ForeignKey("photos.id")),
+    Column("tags", ForeignKey("tags.id")),
+)
+
+
+class BaseTable(Base):
+    __abstract__ = True
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, onupdate=datetime.now)
+
+
+class Tag(Base):
+    __tablename__ = 'tags'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(40), nullable=False)
+
+
+class Comment(BaseTable):
+    __tablename__ = 'comments'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    text: Mapped[str] = mapped_column(String(255), nullable=False)
+    photo_id: Mapped[int] = mapped_column(ForeignKey("photos.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+
+class Photo(BaseTable):
+    __tablename__ = 'photos'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    photo_url: Mapped[str] = mapped_column(String(255), nullable=False)
+    changed_photo_url: Mapped[str] = mapped_column(String(255), nullable=True)
+    tags: Mapped[list[Tag]] = relationship("Tag", secondary="association_table", backref="photos")
+    comments: Mapped[list[Comment]] = relationship("Comment")
+
+
+class User(BaseTable):
+    __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[String] = mapped_column(String(50))
+    email: Mapped[String] = mapped_column(String(100), nullable=False, unique=True)
+    password: Mapped[String] = mapped_column(String(100), nullable=False)
+    role: Mapped[String] = mapped_column(String(50), nullable=False)
+    refresh_token: Mapped[String] = mapped_column(String(255), nullable=True)
+    confirmed: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
+    avatar: Mapped[String] = mapped_column(String(255), nullable=True)
