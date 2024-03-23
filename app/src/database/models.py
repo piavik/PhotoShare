9c7206fce55df7c8b448b7ecfe7fc9fba03b1fd9
lomakina0612 <lomakina0612@gmail.com>
2024-03-23 06:19:15 +0100

    Добавлено отношение к комментариям в модели Photo

--- app/src/database/models.py
+++ app/src/database/models.py
@@ -16,17 +16,19 @@ association_table = Table(
 class BaseTable(Base):
     __abstract__ = True
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
-    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, onupdate=datetime.now)
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.now, onupdate=datetime.now
+    )
 
 
 class Tag(Base):
-    __tablename__ = 'tags'
+    __tablename__ = "tags"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(40), nullable=False)
 
 
 class Comment(BaseTable):
-    __tablename__ = 'comments'
+    __tablename__ = "comments"
     id: Mapped[int] = mapped_column(primary_key=True)
     text: Mapped[str] = mapped_column(String(255), nullable=False)
     photo_id: Mapped[int] = mapped_column(ForeignKey("photos.id"))
@@ -34,26 +36,30 @@ class Comment(BaseTable):
 
 
 class Photo(BaseTable):
-    __tablename__ = 'photos'
+    __tablename__ = "photos"
     id: Mapped[int] = mapped_column(primary_key=True)
     photo_url: Mapped[str] = mapped_column(String(255), nullable=False)
     description: Mapped[str] = mapped_column(String(255), nullable=True)
     owner_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     user = relationship("User", backref="photos")
     changed_photo_url: Mapped[str] = mapped_column(String(255), nullable=True)
-    tags: Mapped[list[Tag]] = relationship("Tag", secondary="association_table", backref="photos")
-    comments: Mapped[list[Comment]] = relationship("Comment")
+    tags: Mapped[list[Tag]] = relationship(
+        "Tag", secondary="association_table", backref="photos"
+    )
+    comments: Mapped[list[Comment]] = relationship(
+        "Comment", backref="photo"
+    )  # Добавлено отношение к комментариям
     rating: Mapped[Float] = mapped_column(Float, nullable=True, default=0.0)
 
 
 class Role(Base):
-    __tablename__ = 'roles'
+    __tablename__ = "roles"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
 
 
 class User(BaseTable):
-    __tablename__ = 'users'
+    __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[String] = mapped_column(String(50))
     email: Mapped[String] = mapped_column(String(100), nullable=False, unique=True)
