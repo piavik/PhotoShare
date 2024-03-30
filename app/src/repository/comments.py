375b27b0adabbf184d9ce7df09b44506dc282b68
piavik <piavik@gmail.com>
2024-03-30 21:15:19 +0100

    src.repo.comments

--- app/src/repository/comments.py
+++ app/src/repository/comments.py
@@ -6,6 +6,16 @@ from app.src.schemas import CommentModel
 
 
 async def update_comment(db: Session, new_comment: CommentModel) -> Comment:
+    """
+    Update comment in the database
+
+    Args:
+        db (Session): database session
+        new_comment (CommentModel): new CommentModel object
+
+    Returns:
+        Comment: Comment object
+    """    
     new_comment = Comment(**new_comment.dict())
 
     db.add(new_comment)
@@ -14,12 +24,23 @@ async def update_comment(db: Session, new_comment: CommentModel) -> Comment:
 
     return new_comment
 
-async def delete_comment(db: Session, photo_id: int, user_id: int):
+async def delete_comment(db: Session, photo_id: int, user_id: int) -> bool:
+    """
+    Delete comment from the database
+
+    Args:
+        db (Session): database session
+        photo_id (int): ID of the commented photo
+        user_id (int): ID of the user making the comment
+
+    Returns:
+        bool: result
+    """    
     comment = (
         db.query(Comment).filter(Comment.photo_id == photo_id, Comment.user_id == user_id).first()
         )
     if not comment:
-        return None
+        return False
     db.delete(comment)
     db.commit()
     return True
