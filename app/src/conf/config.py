99291d5e1ffaa4ea3f62c7d2488fe816648b6fe1
piavik <piavik@gmail.com>
2024-04-03 20:14:47 +0200

    conf fix

--- app/src/conf/config.py
+++ app/src/conf/config.py
@@ -14,8 +14,8 @@ class Settings(BaseSettings):
     mail_from_name: str
     redis_host: str
     redis_port: int
-    redis_user: str = None
-    redis_pass: str = None
+    redis_user: str
+    redis_pass: str
     cloudinary_name: str
     cloudinary_api_key: str
     cloudinary_api_secret: str
